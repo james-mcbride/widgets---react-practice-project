c325d567b7eb983860f216a17ff53de11daac525
james McBride <james.m.mcbride15@gmail.com>
2021-04-03 17:23:12 -0500

    adding entire practice project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const items=[
+    {
+        title: 'What is React?',
+        content: 'Reacgt is a front end js frameowkr'
+    },
+    {
+        title: 'Why use react?',
+        content: 'React is a js developers fave'
+    },
+    {
+        title: 'How do you use react?',
+        content: 'You use components'
+    }
+]
+
+const options = [
+    {
+        label: 'Afrikaans',
+        value: 'af'
+    },
+    {
+        label: '',
+        value: ''
+    },
+    {
+        label: "Hindi",
+        value: 'hi'
+    }
+]
+
+
+
+export default () =>{
+
+    const [selected, setSelected]=useState(0)
+
+    return <div>
+        <Header />
+        <Route path="/">
+            <Accordion items={items} />
+        </Route>
+        <Route path="/list">
+            <Search />
+        </Route>
+        <Route path="/dropdown">
+            <Dropdown
+                label="select a color"
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected}
+            />
+        </Route>
+        <Route path="/translate">
+            <Translate />
+        </Route>
     </div>
-  );
 }
-
-export default App;
